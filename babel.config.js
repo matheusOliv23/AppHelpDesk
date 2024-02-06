b0b811eaad165738af5ba08e88839ab958d63683
matheusOliv23 <matheushenri1996@gmail.com>
2024-02-06 06:22:32 -0300

    feat: add initial repo configs, routes and styles

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,33 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: [
+            '.ios.js',
+            '.android.js',
+            '.ios.jsx',
+            '.android.jsx',
+            '.js',
+            '.jsx',
+            '.json',
+            '.ts',
+            '.tsx',
+          ],
+          root: ['.'],
+          alias: {
+            '@api': './src/api',
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@scenes': './src/scenes',
+            '@theme': './src/theme',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
