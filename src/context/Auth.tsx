3dd5c8253d7bfd6c72e887284fa390003d2b3a8a
matheusOliv23 <matheushenri1996@gmail.com>
2024-02-07 07:25:45 -0300

    feat: implement login persisti in storage

--- src/context/Auth.tsx
+++ src/context/Auth.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AuthService } from 'src/services/auth.service';
 
 export interface AuthData {
@@ -9,6 +10,7 @@ interface AuthContextDataProps {
   authData?: AuthData;
   signIn: (email: string, password: string) => Promise<AuthData>;
   signOut: () => Promise<void>;
+  loading: boolean;
 }
 
 export const AuthContext = createContext<AuthContextDataProps>(
@@ -17,20 +19,35 @@ export const AuthContext = createContext<AuthContextDataProps>(
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [authData, setAuthData] = useState<AuthData>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loadFromStorage();
+  }, []);
+
+  async function loadFromStorage() {
+    const auth = await AsyncStorage.getItem('@token');
+    if (auth) {
+      setAuthData(JSON.parse(auth));
+    }
+    -setLoading(false);
+  }
 
   async function signIn(email: string, password: string): Promise<AuthData> {
     const auth = await AuthService.signIn(email, password);
     setAuthData(auth);
+    AsyncStorage.setItem('@token', JSON.stringify(auth));
     return auth;
   }
 
   async function signOut(): Promise<void> {
-    const auth = await AuthService.signOut();
+    await AuthService.signOut();
     setAuthData(undefined);
-    return auth;
+    AsyncStorage.removeItem('@token');
   }
+
   return (
-    <AuthContext.Provider value={{ authData, signIn, signOut }}>
+    <AuthContext.Provider value={{ authData, signIn, signOut, loading }}>
       {children}
     </AuthContext.Provider>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,10 +3,13 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppRoutes } from './AppStack';
 import { useAuth } from 'src/context/Auth';
 import { AuthRoutes } from './AuthStack';
+import { Loading } from 'src/components/shared/Loading';
 
 export function Routes() {
   const queryClient = new QueryClient();
-  const { authData } = useAuth();
+  const { authData, loading } = useAuth();
+
+  if (loading) return <Loading />;
 
   return (
     <NavigationContainer>
