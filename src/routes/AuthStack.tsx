eeb2ad17588d8cb686fa1d1015db9ef7d53cab89
matheusOliv23 <matheushenri1996@gmail.com>
2024-02-07 20:57:50 -0300

    feat: add new routes

--- src/routes/AuthStack.tsx
+++ src/routes/AuthStack.tsx
@@ -1,17 +1,76 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 import { View } from 'react-native';
 import { useTheme } from 'styled-components/native';
-import Home from 'src/screens/Auth/Home';
+
+import HeaderTitle from 'src/components/Home/HeaderTitle';
+import Waiting from 'src/screens/Auth/Home/Waiting';
+import Finished from 'src/screens/Auth/Home/Finished';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Solicitation from 'src/screens/Auth/Solicitation';
+
+import HeaderSolicitation from 'src/components/Solicitation/HeaderSolicitation';
 
 const { Navigator, Screen } = createNativeStackNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 export function AuthRoutes() {
   const { COLORS } = useTheme();
+
+  function Home() {
+    return (
+      <Tab.Navigator
+        style={{ backgroundColor: COLORS.GRAY_700 }}
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: COLORS.GRAY_700,
+            borderBottomColor: COLORS.YELLOW_500,
+          },
+          tabBarLabelStyle: {
+            borderWidth: 1,
+            color: COLORS.WHITE,
+            fontSize: 10,
+          },
+          tabBarActiveTintColor: 'transparent',
+          tabBarIndicatorStyle: {
+            backgroundColor: 'transparent',
+          },
+        }}
+      >
+        <Tab.Screen name='Em andamento' component={Waiting} />
+        <Tab.Screen name='Finalizados' component={Finished} />
+      </Tab.Navigator>
+    );
+  }
+
   return (
-    <View style={{ flex: 1, backgroundColor: COLORS.GRAY_700 }}>
+    <View style={{ flex: 1, backgroundColor: COLORS.GRAY_600 }}>
       <Navigator>
-        <Screen name='home' component={Home} />
+        <Screen
+          name='home'
+          component={Home}
+          options={{
+            headerStyle: { backgroundColor: COLORS.GRAY_300 },
+            header: () => (
+              <SafeAreaView>
+                <HeaderTitle />
+              </SafeAreaView>
+            ),
+          }}
+        />
+        <Screen
+          name='Solicitação'
+          component={Solicitation}
+          options={{
+            header: () => (
+              <SafeAreaView>
+                <HeaderSolicitation />
+              </SafeAreaView>
+            ),
+          }}
+        />
       </Navigator>
     </View>
   );
