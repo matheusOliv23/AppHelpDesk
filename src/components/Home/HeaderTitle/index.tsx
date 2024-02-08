82fd61a366cc8a8f5c41bfb93dcfd7ab5d5cdc06
matheusOliv23 <matheushenri1996@gmail.com>
2024-02-07 22:25:26 -0300

    feat: add signout to button

--- src/components/Home/HeaderTitle/index.tsx
+++ src/components/Home/HeaderTitle/index.tsx
@@ -3,14 +3,16 @@ import { Image, Pressable, View, Text } from 'react-native';
 import * as S from './styles';
 import Title from 'src/components/shared/Title';
 import { useTheme } from 'styled-components/native';
+import { useAuth } from 'src/context/Auth';
 
 export default function HeaderTitle() {
   const { COLORS } = useTheme();
+  const { signOut } = useAuth();
   return (
     <View>
       <S.HeaderContainer>
         <Image source={require('src/assets/logo_horizontal.png')} />
-        <Pressable>
+        <Pressable onPress={signOut}>
           <Image source={require('src/assets/icons/sign-out.png')} />
         </Pressable>
       </S.HeaderContainer>
