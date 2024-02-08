b5b4ac29f02c02228fee7f968d81f4d8c17966f7
matheusOliv23 <matheushenri1996@gmail.com>
2024-02-08 06:46:57 -0300

    feat: add cardFilled component

--- src/components/shared/Card/index.tsx
+++ src/components/shared/Card/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Pressable } from 'react-native';
 import * as S from './styles';
 import { useTheme } from 'styled-components/native';
 import { formatDateToDayMonthYearTime } from 'src/utils/date-formatters';
@@ -9,24 +9,35 @@ interface Props {
   time: string;
   icon: ReactNode;
   borderLeftColor?: string;
+  onPress: () => void;
 }
 
-export default function Card({ title, time, icon, borderLeftColor }: Props) {
+export default function Card({
+  title,
+  time,
+  icon,
+  borderLeftColor,
+  onPress,
+}: Props) {
   const { COLORS } = useTheme();
   return (
-    <S.Container borderLeftColor={borderLeftColor}>
-      <View>
-        <Text style={{ color: COLORS.GRAY_100, fontSize: 14, marginBottom: 4 }}>
-          {title}
-        </Text>
-        <View style={{ flexDirection: 'row', gap: 6, alignItems: 'center' }}>
-          <Image source={require('src/assets/icons/clock.png')} />
-          <Text style={{ color: COLORS.GRAY_200, fontSize: 10 }}>
-            {formatDateToDayMonthYearTime(time)}
+    <Pressable onPress={onPress}>
+      <S.Container borderLeftColor={borderLeftColor}>
+        <View>
+          <Text
+            style={{ color: COLORS.GRAY_100, fontSize: 14, marginBottom: 4 }}
+          >
+            {title}
           </Text>
+          <View style={{ flexDirection: 'row', gap: 6, alignItems: 'center' }}>
+            <Image source={require('src/assets/icons/clock.png')} />
+            <Text style={{ color: COLORS.GRAY_200, fontSize: 10 }}>
+              {formatDateToDayMonthYearTime(time)}
+            </Text>
+          </View>
         </View>
-      </View>
-      <S.IconContainer>{icon}</S.IconContainer>
-    </S.Container>
+        <S.IconContainer>{icon}</S.IconContainer>
+      </S.Container>
+    </Pressable>
   );
 }
