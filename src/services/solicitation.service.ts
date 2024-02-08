3ad396eeb4ac75b863af52f0a2e133b16c8877aa
matheusOliv23 <matheushenri1996@gmail.com>
2024-02-07 23:55:07 -0300

    feat: add solicitations service

--- src/services/solicitation.service.ts
+++ src/services/solicitation.service.ts
@@ -4,4 +4,22 @@ const createSolicitation = async (solicitation: ISolicitation) => {
   api.post('/tickets', solicitation);
 };
 
-export const SolicitationService = { createSolicitation };
+const getSolicitations = async (page: string) => {
+  const res = await api.get(`/tickets?page=${page}`);
+  return res.data;
+};
+
+const getSolicitationById = async (id: string) => {
+  const res = await api.get(`/tickets/${id}`);
+  return res.data;
+};
+
+const closeSolicitation = async (id: string, solution: string) =>
+  api.put(`/tickets/${id}`, { solution });
+
+export const SolicitationService = {
+  createSolicitation,
+  getSolicitations,
+  getSolicitationById,
+  closeSolicitation,
+};
