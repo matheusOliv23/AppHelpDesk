86178a3045d823f69826955c38fa6b2be9fddcc4
matheusOliv23 <matheushenri1996@gmail.com>
2024-02-07 23:54:53 -0300

    feat: implementing solicitations open list

--- src/types/Solicitation.ts
+++ src/types/Solicitation.ts
@@ -2,3 +2,13 @@ interface ISolicitation {
   title: string;
   description: string;
 }
+
+interface ISolicitationResponse {
+  id: string;
+  status: 'open' | 'closed';
+  description: string;
+  title: string;
+  created_at: string;
+  updated_at: string;
+  closed_at: string;
+}
